71ef2cfc1f693e5078784d42a8d6b866b7be74ff
Andrew-Kluge <andrewkluge2017@u.northwestern.edu>
2018-02-17 15:46:20 -0600

    Got searching the tweets for terms worked out.  Moved some files into a Helper functions folder.  Created clean_dictionary (currently blank) to clean the dict of {term: names}
    TODO:  Write clean_dictionary (combine like terms, put into dict of counts)
    after that, idea of what we need to finish:  select the name with the highest count and place it as the winner, then need to refine search terms we are using and add any other logic

--- tweet_parser.py
+++ tweet_parser.py
@@ -29,27 +29,42 @@ information = {"Host": [],
                "Best Performance by an Actress in a Supporting Role in a Series, Limited Series or Motion Picture Made for Television": [],
                }
 
+term_results = {"Host": [], "Presenter": [], "Best Actor": [], "Greatest Actor": []}
+
 text_file = open("Files\\ggTweets.txt", "r")
 tweets = text_file.readlines()
 
 placeholder = 0
+hit_count = 0
 num_exceptions = 0
-for i in range(0, len(search_terms.keys())):
-    for term in search_terms.get(search_terms.keys()[i]): #range(0, len(search_terms.get(search_terms.keys()[i]))):
-        for t in tweets:
+# Now we go very fast
+for t in tweets:
+    got_names = False
+    # while(got_names == False):
+    for i in range(0, len(search_terms.keys())):
+        for term in search_terms.get(search_terms.keys()[i]): #range(0, len(search_terms.get(search_terms.keys()[i]))):
             if term in t:
+                    # got_names = True
                 try:
+                    hit_count += 1
                     name_list = get_human_names(t)
+                    for name in name_list:
+                        term_results.setdefault(term, []).append(name)
+
                 except:
                     num_exceptions += 1
-                placeholder += 1
-                if(placeholder%10 == 0):
-                    print(placeholder)
-                ## extract name
-print (placeholder)
+    placeholder += 1
+    if(placeholder%1000 == 0): # Now represents going through 1000 tweets
+        print(placeholder)
+    if(placeholder%100000 == 0): ## To save time when testing
+        break
 
+print (placeholder)
+print (hit_count)
+print (num_exceptions)
 print(search_terms.values())
 print(information.keys())
+print(term_results)
 
 
 #### Too Slow ####
